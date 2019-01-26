2606fa2d1769c2d08583b91966371bac1311acdc
Jubke <luebke.julian@gmail.com>
2019-01-26 12:15:12 +0100

    4-windows-lstm-10-fold

--- aatm_support.py
+++ aatm_support.py
@@ -1,6 +1,7 @@
 """ Helper functions for AATM seminar topic I code base. """
 import os
 import sys
+import matplotlib.pyplot as plt
 
 ###############
 # File helpers
@@ -26,6 +27,51 @@ def last_file(base, extension='.txt'):
     return f'{base}_{i - 1}{extension}'
 
 ####################################
+# Keras visualisation
+def draw_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.figure(1)
+
+    ax = plt.subplot(211)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # acc_values = acc
+    # val_acc_values = val_acc
+
+    ax2 = plt.subplot(212)
+    ax2.spines["top"].set_visible(False)
+    ax2.spines["right"].set_visible(False)
+    plt.plot(epochs, acc, 'b', label='Training acc')
+    plt.plot(epochs, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplots_adjust(
+        left = 0.125,
+        right = 0.9,
+        bottom = 0.1,
+        top = 0.9,
+        wspace = 0.2,
+        hspace = 0.2
+    )
+    plt.show()
+
+# ####################################
 # Utility functions for development
 
 def current_ipython_memory():
